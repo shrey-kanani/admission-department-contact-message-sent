31f61790660d3d4c0555b03804eca6696c8ab6d9
Shrey Kanani <shreykanani1003@gmail.com>
2025-02-06 15:25:14 +0530

    Message with File

--- bulkMessageForm.js
+++ bulkMessageForm.js
@@ -5,63 +5,254 @@ $(document).ready(function () {
   ) {
     $("#apiForm").submit(function (event) {
       event.preventDefault();
+
+      // STEP - 1 VARIABLES
       var apiKey =
         "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwaG9uZU51bWJlciI6Iis5MTcwOTY5Nzk5MDAiLCJwaG9uZU51bWJlcklkIjoiMjMxMTgxOTYzNDE0MjQ2IiwiaWF0IjoxNzA4MDYwMDg3fQ.vkXWdDOEW1mCRPdiaNdNshOFkIn0tPM-E_SmuDnMtiw";
       var customMessage = $("#customMessage").val();
+      if (customMessage == "") {
+        customMessage = "Please find attached file.";
+      }
       var numbersCSV = $("#numbersCSV").val();
+      var fileInput = $("#customFile")[0].files;
 
       const numberArray = numbersCSV.split(",");
 
-      numberArray.forEach((number) => {
-        const trimmedNumber = number.trim();
-        $.ajax({
-          url: "https://wb-api.chatomate.in/whatsapp-cloud/messages", // Replace apiEndpoint with your actual API endpoint
-          method: "POST", // Adjust the method as needed (POST, GET, etc.)
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: apiKey,
-          },
-          data: JSON.stringify({
-            to: "91" + trimmedNumber,
-            type: "template",
-            source: "external",
-            template: {
-              name: "admin_general_meesage_template",
-              language: {
-                code: "en",
-              },
-              components: [
-                {
-                  type: "body",
-                  parameters: [
-                    {
-                      type: "text",
-                      text: customMessage,
-                    },
-                  ],
-                },
-              ],
+      // STEP - 2 SEND FOR FILE ATTACHED
+      if (fileInput.length > 0) {
+        var file = fileInput[0]; // Get the selected file
+        var allowedTypes = [
+          "image/jpeg",
+          "image/png",
+          "application/pdf",
+          "application/xlsx",
+          "application/msword",
+          "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+          "application/vnd.ms-excel",
+          "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+        ];
+
+        // Check if the file is an image or a PDF
+        if (!allowedTypes.includes(file.type)) {
+          alert("Only image files (JPG, PNG, XLSX, WORD or PDF are allowed!");
+          return;
+        }
+
+        var fileInput = document.getElementById("customFile");
+
+        var formData = new FormData();
+        formData.append("file", fileInput.files[0]);
+
+        console.log(file.type);
+
+        // STEP - 2.1 UPLOAD MEDIA
+        if (file.type == "application/pdf") {
+          $.ajax({
+            url: "https://wb-api.chatomate.in/whatsapp-cloud/media",
+            method: "POST",
+            headers: {
+              Authorization: apiKey, // Use your actual API key
             },
-          }),
-          success: function (response) {
-            $("#result").html(
-              `<div class="alert alert-success"><strong>Success!</strong> Message Sent Successfully.</div>`
-            );
-          },
-          error: function (xhr, status, error) {
-            $("#result").html(
-              `<div class="alert alert-danger"><strong>Failed!</strong> Some error occured</div>`
-            );
-          },
+            data: formData,
+            processData: false, // Important: Prevent jQuery from processing data
+            contentType: false, // Important: Prevent jQuery from setting content type
+            success: function (response) {
+              console.log(String(response.id));
+              console.log(customMessage);
+
+              // STEP - 2.2 SEND MESSAGE WITH MEDIA ID
+              numberArray.forEach((number) => {
+                const trimmedNumber = number.trim();
+                $.ajax({
+                  url: "https://wb-api.chatomate.in/whatsapp-cloud/messages", // Replace apiEndpoint with your actual API endpoint
+                  method: "POST", // Adjust the method as needed (POST, GET, etc.)
+                  headers: {
+                    "Content-Type": "application/json",
+                    Authorization: apiKey,
+                  },
+                  data: JSON.stringify({
+                    to: "91" + trimmedNumber,
+                    type: "template",
+                    source: "external",
+                    template: {
+                      name: "admin_general_meesage_template_with_files",
+                      language: {
+                        code: "en",
+                      },
+                      components: [
+                        {
+                          type: "header",
+                          parameters: [
+                            {
+                              type: "document",
+                              document: {
+                                id: String(response.id),
+                                filename: "my_file",
+                              },
+                            },
+                          ],
+                        },
+                        {
+                          type: "body",
+                          parameters: [
+                            {
+                              type: "text",
+                              text: customMessage,
+                            },
+                          ],
+                        },
+                      ],
+                    },
+                  }),
+                  success: function (response) {
+                    $("#result").html(
+                      `<div class="alert alert-success"><strong>Success!</strong> Message Sent Successfully.</div>`
+                    );
+                    setTimeout(() => {
+                      location.reload();
+                    }, 5000);
+                  },
+                  error: function (xhr, status, error) {
+                    $("#result").html(
+                      `<div class="alert alert-danger"><strong>Failed!</strong> Some error occured</div>`
+                    );
+                  },
+                });
+              });
+            },
+            error: function (xhr, status, error) {
+              console.error("Error:", error);
+            },
+          });
+        } else {
+          $.ajax({
+            url: "https://wb-api.chatomate.in/whatsapp-cloud/media",
+            method: "POST",
+            headers: {
+              Authorization: apiKey, // Use your actual API key
+            },
+            data: formData,
+            processData: false, // Important: Prevent jQuery from processing data
+            contentType: false, // Important: Prevent jQuery from setting content type
+            success: function (response) {
+              console.log(String(response.id));
+              console.log(customMessage);
+
+              // STEP - 2.2 SEND MESSAGE WITH MEDIA ID
+              numberArray.forEach((number) => {
+                const trimmedNumber = number.trim();
+                $.ajax({
+                  url: "https://wb-api.chatomate.in/whatsapp-cloud/messages", // Replace apiEndpoint with your actual API endpoint
+                  method: "POST", // Adjust the method as needed (POST, GET, etc.)
+                  headers: {
+                    "Content-Type": "application/json",
+                    Authorization: apiKey,
+                  },
+                  data: JSON.stringify({
+                    to: "91" + trimmedNumber,
+                    type: "template",
+                    source: "external",
+                    template: {
+                      name: "admin_general_meesage_template_with_img",
+                      language: {
+                        code: "en",
+                      },
+                      components: [
+                        {
+                          type: "header",
+                          parameters: [
+                            {
+                              type: "image",
+                              image: {
+                                id: String(response.id),
+                              },
+                            },
+                          ],
+                        },
+                        {
+                          type: "body",
+                          parameters: [
+                            {
+                              type: "text",
+                              text: customMessage,
+                            },
+                          ],
+                        },
+                      ],
+                    },
+                  }),
+                  success: function (response) {
+                    $("#result").html(
+                      `<div class="alert alert-success"><strong>Success!</strong> Message Sent Successfully.</div>`
+                    );
+                    setTimeout(() => {
+                      location.reload();
+                    }, 5000);
+                  },
+                  error: function (xhr, status, error) {
+                    $("#result").html(
+                      `<div class="alert alert-danger"><strong>Failed!</strong> Some error occured</div>`
+                    );
+                  },
+                });
+              });
+            },
+            error: function (xhr, status, error) {
+              console.error("Error:", error);
+            },
+          });
+        }
+      } else {
+        numberArray.forEach((number) => {
+          const trimmedNumber = number.trim();
+          $.ajax({
+            url: "https://wb-api.chatomate.in/whatsapp-cloud/messages", // Replace apiEndpoint with your actual API endpoint
+            method: "POST", // Adjust the method as needed (POST, GET, etc.)
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: apiKey,
+            },
+            data: JSON.stringify({
+              to: "91" + trimmedNumber,
+              type: "template",
+              source: "external",
+              template: {
+                name: "admin_general_meesage_template",
+                language: {
+                  code: "en",
+                },
+                components: [
+                  {
+                    type: "body",
+                    parameters: [
+                      {
+                        type: "text",
+                        text: customMessage,
+                      },
+                    ],
+                  },
+                ],
+              },
+            }),
+            success: function (response) {
+              $("#result").html(
+                `<div class="alert alert-success"><strong>Success!</strong> Message Sent Successfully.</div>`
+              );
+              setTimeout(() => {
+                location.reload();
+              }, 5000);
+            },
+            error: function (xhr, status, error) {
+              $("#result").html(
+                `<div class="alert alert-danger"><strong>Failed!</strong> Some error occured</div>`
+              );
+            },
+          });
         });
-      });
+      }
 
       document.getElementById("apiForm").reset();
       setFocusToFirstField();
-
-      setTimeout(() => {
-        location.reload();
-      }, 2000);
     });
   } else {
     window.location.href = "index.html";
